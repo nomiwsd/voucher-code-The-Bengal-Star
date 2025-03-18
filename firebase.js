7b783465d87982787571fcf0c53be512820cdf22
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2025-03-18 05:22:53 +0500

    new changes

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD6XnoYptZRyB80M2_dxDuMpiOsvvrFkjQ",
+  authDomain: "voucher-code-the-bengal-stars.firebaseapp.com",
+  projectId: "voucher-code-the-bengal-stars",
+  storageBucket: "voucher-code-the-bengal-stars.firebasestorage.app",
+  messagingSenderId: "778883283357",
+  appId: "1:778883283357:web:26d7216df5fb16a3cd5774",
 };
 
 // Initialize Firebase
